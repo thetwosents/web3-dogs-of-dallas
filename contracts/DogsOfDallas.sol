deb98a53902d3f08f578d2bdc9cd90cd08f20e01
Jon Senterfitt <thetwosents@gmail.com>
2022-04-19 18:01:28 -0500

    Compiled contract to test Ganache workflow

--- contracts/DogsOfDallas.sol
+++ contracts/DogsOfDallas.sol
@@ -29,7 +29,7 @@ contract DogsOfDallas {
     uint dogs_deleted;
     uint dogs_viewed;
     
-    function DogsOfDallas() {
+    function DogsOfDallasConstruct() public {
         dogs_count = 0;
         dogs_registered = 0;
         dogs_updated = 0;
@@ -60,20 +60,21 @@ contract DogsOfDallas {
         dog.breeds = _breeds;
     }
 
-    // This function allows owners of dogs in the city of Dallas to delete their dogs' profiles on the blockchain.
-    function deleteDog(uint _id) public {
+    // Delete a dog by owner address and dog id
+    function deleteDog(address _owner, uint _dog_id) public {
         dogs_deleted++;
-        dogs[_id].id = 0;
-        dogs[_id].name = "";
-        dogs[_id].birthdate = "";
-        dogs[_id].breeds = new string[](0);
-        dogs[_id].owners = new address[](0);
+        Dog memory dog = dogs[_dog_id - 1];
+        require(dog.owners[0] == _owner, "Only the owner of a dog can delete it");
     }
 
-    // This function allows owners of dogs in the city of Dallas to view their dogs' profiles on the blockchain.
-    function viewDog(uint _id) public view returns (uint, string, string, string[], address[]) {
-        dogs_viewed++;
-        return (dogs[_id].id, dogs[_id].name, dogs[_id].birthdate, dogs[_id].breeds, dogs[_id].owners);
+    // View a dog by owner address and dog id
+    function viewDog(address _owner, uint _dog_id) public view returns (string memory _name, string memory _birthdate, string[] memory _breeds) {
+        // dogs_viewed++;
+        Dog memory dog = dogs[_dog_id - 1];
+        require(dog.owners[0] == _owner, "Only the owner of a dog can view it");
+        _name = dog.name;
+        _birthdate = dog.birthdate;
+        _breeds = dog.breeds;
     }
 
     // This function allows owners of dogs in the city of Dallas to view the number of dogs registered on the blockchain.
